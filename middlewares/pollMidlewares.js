f3bd779056ae1d2d86df18a0dc3c01c3eba0f4f3
Driven Education <marcio.garrido.jr@gmail.com>
2022-05-19 13:38:12 -0300

    add a function do auto complete the expiration date

--- middlewares/pollMidlewares.js
+++ middlewares/pollMidlewares.js
@@ -1,7 +1,11 @@
+import dayjs from "dayjs";
 import { pollSchema } from "../schemas/pollSchemas.js";
 
 export function validatePoll(req, res, next){
     const { error } = pollSchema.validate(req.body)
+    if(!req.body.expireAt){
+       req.body.expireAt = dayjs().add(30, 'day').format('YYYY-MM-DD HH:mm').toString()
+    }
     if(error){
         return res.sendStatus(422)
     }
