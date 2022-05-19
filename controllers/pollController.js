ef3fe0226605da23171e039a0f3f4e975980c0d7
Driven Education <marcio.garrido.jr@gmail.com>
2022-05-19 13:54:46 -0300

    implementing a function to show a array of polls

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -11,3 +11,16 @@ export async function pollRegister(req, res){
     }
     
 }
+
+export async function showPolls(req, res){
+    try{
+        const polls = await db.collection("polls").find().toArray()
+        await res.send(polls)
+        
+        
+    }
+    catch{
+        console.log("Erro ao encontrar enquetes")
+        res.sendStatus(500)
+    }
+}
--- routes/poll.js
+++ routes/poll.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { pollRegister } from "../controllers/pollController.js";
+import { pollRegister, showPolls } from "../controllers/pollController.js";
 import { validatePoll } from "../middlewares/pollMidlewares.js";
 
 
 
 const poll = Router()
 poll.post("/poll", validatePoll, pollRegister);
+poll.get("/poll", showPolls)
 
 export default poll;
